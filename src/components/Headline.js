47cecbb256f41eb6b056232922cf1f0803988892
ziwei wu <18145625+ziweiwu@users.noreply.github.com>
2018-05-23 11:18:13 -0400

    revert to earlier version

---
+++ src/components/Headline.js
@@ -0,0 +1,18 @@
+import React from 'react';
+
+const Headline = (props) => {
+  return (
+    <div>
+      <h3> {props.title}</h3>
+      {props.subtitle && <h4>{props.subtitle}</h4>}
+    </div>
+  );
+};
+
+Headline.defaultProps = {
+  title: 'Title'
+}
+
+export default Headline
+
+
