8276cb1a30da0873e0e962d3aa8aa60c79b85cf0
ziwei wu <18145625+ziweiwu@users.noreply.github.com>
2018-05-24 16:27:41 -0400

    start working on date picker component

--- src/App.js
+++ src/App.js
@@ -3,13 +3,22 @@ import './App.css';
 import "@hig/styles/build/index.css";
 import TableView from './components/TableView';
 import PickDate from './components/PickDate'
+import {Container, Row, Col} from 'react-grid-system'
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <TableView/>
-        <PickDate/>
+        <Container fluid/>
+        <Row>
+          <Col sm={2} className="side-area"></Col>
+          <Col sm={8}>
+            <TableView/>
+            <PickDate/>
+          </Col>
+          <Col sm={2} className="side-area"></Col>
+        </Row>
+        <Container/>
       </div>
     );
   }
