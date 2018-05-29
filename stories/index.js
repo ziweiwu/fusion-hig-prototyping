4a56f2bc8042ff2f822665a4a7d5f6877c6c4a8c
ziwei wu <18145625+ziweiwu@users.noreply.github.com>
2018-05-29 16:00:20 -0400

    fixed the spacing issue between input field and calender

--- stories/index.js
+++ stories/index.js
@@ -4,10 +4,10 @@ import PickDate from '../src/components/PickDate.js';
 import DatePicker from "../src/components/DatePicker";
 
 storiesOf('Date Picker', module)
-  .add('Only Date ', () =>(<DatePicker label="Pick a date" />), {notes: 'Default Date Picker'})
-  .add('Portal', () =>(<DatePicker withPortal label = "With Portal"/>), {notes: 'Portal Date Picker'})
+  .add('Default View', () =>(<DatePicker label="Pick a date" />), {notes: 'Default View'})
+  .add('Portal View', () =>(<DatePicker withPortal label = "With Portal"/>), {notes: 'Portal View'})
   .add('Disabled', () =>(<DatePicker label="Disabled" disabled={true}/>), {notes: 'Disabled'})
-  .add('With Placeholder Text', () =>(<DatePicker label="With Placehold Text" placeholderText="Please Pick a date"/>), {notes: 'With Place Holder'})
+  .add('With Placeholder Text', () =>(<DatePicker label="With Placehold Text" placeholderText="Please Pick a date"/>), {notes: 'With Place Holder Text'})
 
 
 ;
