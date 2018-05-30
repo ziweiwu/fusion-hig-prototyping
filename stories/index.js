eb89270d7a5683655f1c8082aa7f39c9409a1bf2
t_wuz <ziwei.wu@autodesk.com>
2018-05-30 17:11:34 -0400

    added some storyboards

--- stories/index.js
+++ stories/index.js
@@ -6,18 +6,30 @@ import {withKnobs, text, boolean, number, select} from '@storybook/addon-knobs';
 import DatePicker from "../src/components/DatePicker";
 import PickDateRange from "../src/components/PickDateRange";
 import DisableDates from "../src/components/DisableDates";
+import Locale from "../src/components/Locale";
+import CustomDateFormat from "../src/components/CustomDateFormat";
 
 const stories = storiesOf('Date Picker', module);
 stories.addDecorator(withKnobs);
 
-//defeault view
+//default view
 stories.add('Default View', () => (<DatePicker label="Pick a date" selected={moment()}/>), {notes: 'Default View'});
 
 //portal view
 stories.add('Portal View', () => (<DatePicker withPortal label="With Portal"/>), {notes: 'Portal View'});
 
 //disabled
-stories.add('Disabled', () => (<DatePicker label="Disabled" disabled={true}/>), {notes: 'Disabled'});
+stories.add('Disabled', () => {
+    const disable_label = 'Disable State';
+    const enable_label = 'Enabled';
+    const defaultValue = true;
+    const disable = boolean('Disabled State', defaultValue);
+
+    return (
+      (<DatePicker label={disable ? disable_label : enable_label} disabled={disable}/>)
+    )
+  }
+);
 
 //with placeholder text
 stories.add('With Placeholder Text', () => (
@@ -30,15 +42,26 @@ stories.add('Select a Range of Dates', () => (<PickDateRange/>), {notes: 'Select
 //disable certain days
 stories.add('Disable Certain Days', () => (<DisableDates/>), {notes: 'Disable Certain Days'});
 
-//locale
+//Change calender locale
 stories.add('Locale', () => {
   const label = 'Locale';
-  const options ={French: 'fr-ca', English: 'en-us', Chinese: 'zh-cn', Japanese: 'ja'};
+  const options = ['fr-ca', 'en-us', 'zh-cn', 'ja'];
   const defaultValue = 'fr-ca';
   const locale = select(label, options, defaultValue);
 
   return (
-    (<DatePicker label="Calender with Locale" selected={moment()} locale={locale}/>)
+    (<Locale label="Calender with Locale" locale={locale}/>)
   )
 });
 
+//custom date format
+stories.add('Custom Date Format', () => {
+  const label = 'Date Format';
+  const options = ['L', 'ddd,MMM', 'YYYY-MM-DD', 'DD-MM-YY'];
+  const defaultValue = 'L';
+  const dateFormat = select(label, options, defaultValue);
+
+  return (
+    (<CustomDateFormat label="Input with Custom Date Format" dateFormat={dateFormat}/>)
+  )
+});
