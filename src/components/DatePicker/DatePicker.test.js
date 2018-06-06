2b74c60a77c6b6051d99738a3db086c4aa127081
ziwei wu <18145625+ziweiwu@users.noreply.github.com>
2018-06-06 12:56:36 -0400

    wrote some basic testing

--- src/components/DatePicker/DatePicker.test.js
+++ src/components/DatePicker/DatePicker.test.js
@@ -2,57 +2,66 @@ import React from "react";
 import {mount, shallow} from "enzyme";
 import moment from "moment";
 import DatePicker from "./DatePicker";
-import sinon from "sinon";
-import TestWrapper from "./TestWrapper"
 import propagateToGlobal from "./test_setup"
+import sinon from 'sinon';
 
-//propagate DOM for testing with mount
 propagateToGlobal();
 
+const fn= jest.fn();
+
 describe("DatePicker", () => {
-  let sandbox;
-
-  beforeEach(() => {
-    sandbox = sinon.sandbox.create();
-  });
-
-  afterEach(() => {
-    sandbox.restore();
-  });
-
   //snapshot testing
-  test('Date Picker passes snapshot testing', () => {
+  test('Snapshot tests regular view', () => {
     const wrapper = shallow(<DatePicker selected={moment("2017-09-15 09:30:00")}/>);
     expect(wrapper).toMatchSnapshot();
   });
 
-  //interaction testing
+  test('Snapshot tests portal view', () => {
+    const wrapper = shallow(<DatePicker selected={moment("2017-09-15 09:30:00")} withPortal/>);
+    expect(wrapper).toMatchSnapshot();
+  });
+
+  test('Snapshot tests disabled view', () => {
+    const wrapper = shallow(<DatePicker selected={moment("2017-09-15 09:30:00")} disabled/>);
+    expect(wrapper).toMatchSnapshot();
+  });
+
+  //component testing
   it("Render the dayPicker component correctly.", () => {
     const wrapper = shallow(<DatePicker/>);
     expect(wrapper).toHaveLength(1);
   });
 
+  it("Render the dayPicker input field correctly.", () => {
+    const wrapper = mount(<DatePicker/>);
+    expect(wrapper.find("TextField").length).toBe(1);
+  });
+
   it("Render the label Correctly.", () => {
-    const wrapper = shallow(<DatePicker label="foo"/>);
-    expect(wrapper.instance().props.label).toEqual("foo");
+    const wrapper = mount(<DatePicker label="foo"/>);
+    expect(wrapper.instance().props.label).toBe("foo");
   });
 
-  it("Render the placeholder text Correctly.", () => {
-    const wrapper = shallow(<DatePicker placeholderText="foo"/>);
-    expect(wrapper.instance().props.placeholderText).toEqual("foo");
+  it("Render the placeholder Correctly.", () => {
+    const wrapper = mount(<DatePicker placeholderText="bar"/>);
+    expect(wrapper.instance().props.placeholderText).toBe("bar");
   });
 
+  //test interactions
   it("Show the calendar when focusing on the date input", () => {
     const wrapper = mount(<DatePicker/>);
-    wrapper.simulate("focus");
-    expect(wrapper.find("Calender").exists()).toEqual(true);
+    const input = wrapper.find("TextField");
+    input.props().onClick();
+    expect(wrapper.find(".react-datepicker-popper").length).toBe(1);
   });
 
-  it("Calender does not show if disable is selected", () => {
+  it("Disable calender popper when disable is selected", () => {
     const wrapper = mount(<DatePicker disabled/>);
-    wrapper.simulate("focus");
-    expect(wrapper.find("Calender").exists()).toEqual(false);
-    });
+    const input = wrapper.find("TextField");
+    input.props().onClick();
+    expect(wrapper.find(".react-datepicker-popper").length).toBe(0);
+  });
+
 });
 
 
