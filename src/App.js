2b0e6f71f3e57df9b573cf17fa2a480087045b40
t_wuz <ziwei.wu@autodesk.com>
2018-06-06 17:10:34 -0400

    working on date picker wrapper

--- src/App.js
+++ src/App.js
@@ -1,23 +1,24 @@
 import React, {Component} from 'react';
 import './App.css';
 import "@hig/styles/build/index.css";
-import {Container, Row, Col} from 'react-grid-system'
-import DatePicker from "./components/DatePicker/DatePicker";
+import {Row, Col} from 'react-grid-system'
+import DatePicker from "./components/myDayPicker/index";
+import moment from "moment"
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <Row>
-          <Col sm={2} className="side-area"></Col>
-          <Col sm={8}>
-            <DatePicker />
-          </Col>
-          <Col sm={2} className="side-area"></Col>
-        </Row>
+          <Row>
+            <Col sm={2} className="side-area"></Col>
+            <Col sm={8}>
+              <DatePicker startTime = {moment()}/>
+            </Col>
+            <Col sm={2} className="side-area"></Col>
+          </Row>
       </div>
-    );
+  );
   }
-}
+  }
 
-export default App;
+  export default App;
