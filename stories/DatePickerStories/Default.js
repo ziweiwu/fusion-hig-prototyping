e89497c4aaf4dbc4728b56820b24dbde07883476
ziwei wu <18145625+ziweiwu@users.noreply.github.com>
2018-06-12 17:36:36 -0400

    allow readOnly for textfield, refactor scss files

--- stories/DatePickerStories/Default.js
+++ stories/DatePickerStories/Default.js
@@ -1,4 +1,4 @@
-import React, { Component } from 'react';
+import React, {Component} from 'react';
 import DatePicker from '../../src/components/DatePicker/index';
 
 export default class Default extends Component {
@@ -6,33 +6,23 @@ export default class Default extends Component {
     super(props);
     this.state = {
       startDate: undefined,
-      endDate: undefined,
     };
     this.handleChangeDate = this.handleChangeDate.bind(this);
-    this.handleChangeStart = this.handleChangeStart.bind(this);
-    this.handleChangeEnd = this.handleChangeEnd.bind(this);
   }
 
   handleChangeDate(date) {
-    this.setState({ startDate: date });
-  }
-
-  handleChangeStart(date) {
-    this.setState({ startDate: date });
-  }
-
-  handleChangeEnd(date) {
-    this.setState({ endDate: date });
+    this.setState({startDate: date});
   }
 
   render() {
     return (
       <div>
         <DatePicker
+          label={this.props.label}
+          placeholderText={this.props.placeholderText}
           selected={this.state.startDate}
           startDate={this.state.startDate}
-          onChange={this.handleChangeStart}
-          label={this.props.label}
+          onChange={this.handleChangeDate}
         />
       </div>
     );
