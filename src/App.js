48f23ab533fd54b18412c2a1cbcae1d491db0848
t_wuz <ziwei.wu@autodesk.com>
2018-06-18 15:29:42 -0400

    initial prototyping done

--- src/App.js
+++ src/App.js
@@ -1,26 +1,66 @@
 import React from 'react';
 import '@hig/styles/build/index.css';
-import TextField from '@hig/text-field';
 import ToolTipHig from '@hig/tooltip';
 import '@hig/tooltip/build/index.css';
-import { DatePicker, Button } from 'antd';
+import {DatePicker, Button, Tooltip as TooltipAnt} from 'antd';
+import YouTube from 'react-youtube';
 import Tooltip from './components/Tooltip/index';
 import 'antd/dist/antd.css'; // or 'antd/dist/antd.less'
 import './App.css';
 
-const App = () => (
+const opts = {
+  height: '390',
+  width: '640',
+  playerVars: { // https://developers.google.com/youtube/player_parameters
+    autoplay: 0,
+  },
+};
+
+  const App = () => (
   <div className="App">
     <h1>App Started </h1>
-    <DatePicker />
-    <TextField readOnly />
+    <TooltipAnt><Button> ToolTip Ant</Button></TooltipAnt>
     <div>
       <ToolTipHig anchorPoint="top-center" content="Testing" >
-        <Button >hig tooltip</Button>
+        <Button>hig tooltip</Button>
       </ToolTipHig>
     </div>
     <div>
-      <Tooltip title="prompt text" placement="bottom" trigger={"hover"}>
-        <Button >ant tooltip</Button>
+      <TooltipAnt title="prompt text" placement="right" mouseLeaveDelay={300}>
+        <Button>Ant Tooltip</Button>
+      </TooltipAnt>
+    </div>
+    <div>
+      <Tooltip
+        autoOverflow = {false}
+        overlay={<div><p>Some instruction is written here with picture</p><img
+          src="https://images.pexels.com/photos/104827/cat-pet-animal-domestic-104827.jpeg?cs=srgb&dl=animal-animal-photography-cat-104827.jpg&fm=jpg"
+        /></div>
+        }
+      >
+        <Button>This is tooltip show picture</Button>
+      </Tooltip>
+      <Tooltip
+        overlay={<p>some text</p>}
+      >
+        <Button>This is tooltip show text</Button>
+      </Tooltip>
+      <Tooltip
+        overlay={<a href="https://www.google.com">link to google</a>}
+      >
+        <Button>This is tooltip with some link</Button>
+      </Tooltip>
+
+      <Tooltip
+        overlay={
+          <YouTube
+            videoId="sRMRQw4Pu60"
+            opts={opts}
+            onReady={this._onReady}
+          />
+          }
+      >
+        <Button>This is tooltip with a video</Button>
       </Tooltip>
     </div>
   </div>
--- src/components/Tooltip/Tooltip.js
+++ src/components/Tooltip/Tooltip.js
@@ -1,22 +1,37 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import classNames from 'classnames';
+import ReactToolTip from 'rc-tooltip';
 import '@hig/styles/build/index.css';
-import '@hig/text-field/build/index.css';
-import { Tooltip as TooltipAnt } from 'antd';
-import 'antd/dist/antd.css'; // or 'antd/dist/antd.less'
-
-const style = {
-  backgroundColor: 'red',
-  color: 'red'
-}
+import {getOverflowOptions, placeArrow} from './Placements';
+import './tooltip.css';
 
 export default class Tooltip extends React.Component {
+  constructor(props) {
+    super(props);
+  }
+
   render() {
     const props = this.props;
-    return (<TooltipAnt
-      style={style}
-      {...props}
-      ref={node => this.node = node}
-    />);
+    return (
+      <ReactToolTip
+        {...props}
+        prefixCls="hig-tooltip"
+        ref={node => this.node = node}
+        trigger="hover"
+        //option to trigger
+        builtinPlacemnts={getOverflowOptions()}
+       // onPopupAlign={placeArrow}
+        //visible
+        placement="bottomLeft"
+        mouseEnterDelay={0.5}
+        mouseLeaveDelay={1000}
+      />
+    );
   }
 }
+
+Tooltip.propTypes = {};
+
+
+
