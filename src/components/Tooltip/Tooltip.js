43d45709d5969790e3486b0fb15847ddf96530ec
t_wuz <ziwei.wu@autodesk.com>
2018-06-18 17:27:45 -0400

    end day

--- src/components/Tooltip/Tooltip.js
+++ src/components/Tooltip/Tooltip.js
@@ -3,12 +3,57 @@ import PropTypes from 'prop-types';
 import classNames from 'classnames';
 import ReactToolTip from 'rc-tooltip';
 import '@hig/styles/build/index.css';
-import {getOverflowOptions, placeArrow} from './Placements';
+import getPlacements, { AdjustOverflow, PlacementsConfig } from './placements';
 import './tooltip.css';
+
+// const align = {
+//   points: ['tc', 'bc'],
+//   offset: [0, -4],
+//   targetOffset: [0, 0],
+//   overflow: { adjustY: 0 },
+// };
 
 export default class Tooltip extends React.Component {
   constructor(props) {
     super(props);
+  }
+
+  onPopupAlign = (domNode: HTMLElement, align: any) => {
+    const placements: any = this.getPlacements();
+    const placement = Object.keys(placements).filter(
+      key => (
+        placements[key].points[0] === align.points[0] &&
+        placements[key].points[1] === align.points[1]
+      ),
+    )[0];
+    if (!placement) {
+      return;
+    }
+    const rect = domNode.getBoundingClientRect();
+    const transformOrigin = {
+      top: '50%',
+      left: '50%',
+    };
+    if (placement.indexOf('top') >= 0 || placement.indexOf('Bottom') >= 0) {
+      transformOrigin.top = `${rect.height - align.offset[1]}px`;
+    } else if (placement.indexOf('Top') >= 0 || placement.indexOf('bottom') >= 0) {
+      transformOrigin.top = `${-align.offset[1]}px`;
+    }
+    if (placement.indexOf('left') >= 0 || placement.indexOf('Right') >= 0) {
+      transformOrigin.left = `${rect.width - align.offset[0]}px`;
+    } else if (placement.indexOf('right') >= 0 || placement.indexOf('Left') >= 0) {
+      transformOrigin.left = `${-align.offset[0]}px`;
+    }
+    domNode.style.transformOrigin = `${transformOrigin.left} ${transformOrigin.top}`;
+  };
+
+  getPlacements() {
+    const { builtinPlacements, arrowPointAtCenter, autoAdjustOverflow } = this.props;
+    return builtinPlacements || getPlacements({
+      arrowPointAtCenter,
+      verticalArrowShift: 8,
+      autoAdjustOverflow,
+    });
   }
 
   render() {
@@ -19,13 +64,13 @@ export default class Tooltip extends React.Component {
         prefixCls="hig-tooltip"
         ref={node => this.node = node}
         trigger="hover"
-        //option to trigger
-        builtinPlacemnts={getOverflowOptions()}
-       // onPopupAlign={placeArrow}
+//        popupAlign={align}
         //visible
-        placement="bottomLeft"
+        placement="top"
         mouseEnterDelay={0.5}
         mouseLeaveDelay={1000}
+        builtinPlacements={this.getPlacements()}
+        onPopupAlign={this.onPopupAlign}
       />
     );
   }
