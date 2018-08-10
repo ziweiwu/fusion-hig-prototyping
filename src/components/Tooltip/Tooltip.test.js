8f94b4f1b00a1c9865f2b134ee8ac28c89cb5ea5
ziweiwu <ziwei.wu@autodesk.com>
2018-08-10 16:28:48 -0400

    add visible state add storybook isVisible option

--- src/components/Tooltip/Tooltip.test.js
+++ src/components/Tooltip/Tooltip.test.js
@@ -1,46 +1,51 @@
-import React from 'react';
-import {shallow} from 'enzyme';
-import Tooltip from './index';
+import React from "react";
+import { shallow } from "enzyme";
+import Tooltip from "./index";
 
-describe('DatePicker', () => {
+describe("DatePicker", () => {
   // snapshot testing
-  test('Snapshot tests right replacement', () => {
-    const wrapper = shallow(<Tooltip title="test" anchorPoint="right" trigger="click">
-      <button>
-        tooltip
-      </button>
-    </Tooltip>);
-    wrapper.simulate('click');
+  test("Snapshot tests right replacement", () => {
+    const wrapper = shallow(
+      <Tooltip title="test" anchorPoint="right" trigger="click">
+        <button>tooltip</button>
+      </Tooltip>
+    );
+    wrapper.simulate("click");
     expect(wrapper).toMatchSnapshot();
   });
 
-  test('Snapshot tests left replacement', () => {
-    const wrapper = shallow(<Tooltip title="test" anchorPoint="left" trigger="click">
-      <button>
-        tooltip
-      </button>
-    </Tooltip>);
-    wrapper.simulate('click');
+  test("Snapshot tests left replacement", () => {
+    const wrapper = shallow(
+      <Tooltip title="test" anchorPoint="left" trigger="click">
+        <button>tooltip</button>
+      </Tooltip>
+    );
+    wrapper.simulate("click");
     expect(wrapper).toMatchSnapshot();
   });
 
-  test('Snapshot tests hover', () => {
-    const wrapper = shallow(<Tooltip title="test" anchorPoint="left" trigger="hover" mouseEnterDelay={0}>
-      <button>
-        tooltip
-      </button>
-    </Tooltip>);
-    wrapper.simulate('hover');
+  test("Snapshot tests hover", () => {
+    const wrapper = shallow(
+      <Tooltip
+        title="test"
+        anchorPoint="left"
+        trigger="hover"
+        mouseEnterDelay={0}
+      >
+        <button>tooltip</button>
+      </Tooltip>
+    );
+    wrapper.simulate("hover");
     expect(wrapper).toMatchSnapshot();
   });
 
-  test('Snapshot tests focus', () => {
-    const wrapper = shallow(<Tooltip title="test" anchorPoint="left" trigger="focus">
-      <button>
-        tooltip
-      </button>
-    </Tooltip>);
-    wrapper.simulate('hover');
+  test("Snapshot tests focus", () => {
+    const wrapper = shallow(
+      <Tooltip title="test" anchorPoint="left" trigger="focus">
+        <button>tooltip</button>
+      </Tooltip>
+    );
+    wrapper.simulate("hover");
     expect(wrapper).toMatchSnapshot();
   });
 
--- src/components/Tooltip/__stories__/stories/TooltipLarge.js
+++ src/components/Tooltip/__stories__/stories/TooltipLarge.js
@@ -1,17 +1,17 @@
-import React from 'react';
-import Button from '@hig/button';
-import YouTube from 'react-youtube';
-import Tooltip from '../../index';
-import '@hig/button/build/index.css';
+import React from "react";
+import Button from "@hig/button";
+import YouTube from "react-youtube";
+import Tooltip from "../../index";
+import "@hig/button/build/index.css";
 
 const TooltipLarge = props => (
   <div>
-    <div style={{ margin: '100px' }} />
+    <div style={{ margin: "100px" }} />
     <span>
-      <div style={{ margin: '100px', float: 'left' }} />
+      <div style={{ margin: "100px", float: "left" }} />
       <Tooltip
         title={props.title}
-        lightTheme={props.lightTheme}
+        isVisible={props.isVisible}
         description={props.description}
         anchorPoint={props.anchorPoint}
         trigger={props.trigger}
@@ -20,7 +20,7 @@ const TooltipLarge = props => (
         linkTitle={props.linkTitle}
         linkURL={props.linkURL}
         width={props.width}
-        content={(
+        content={
           <div>
             <YouTube
               videoId="NSJwq9CVoIk"
@@ -29,12 +29,12 @@ const TooltipLarge = props => (
                 height: props.width * 0.6,
                 playerVars: {
                   autoplay: 0,
-                  rel: 0, // disable relevant videos after playing
-                },
+                  rel: 0 // disable relevant videos after playing
+                }
               }}
             />
           </div>
-      )}
+        }
       >
         <Button title="Tooltip large" />
       </Tooltip>
